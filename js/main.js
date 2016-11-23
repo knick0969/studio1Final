cf179a6c792e09d26209324e173506020b208ed4
Arthur Finkler Freiberger <xpigue@gmail.com>
2016-11-23 04:52:54 +0000

    a few more stuff, getting started on responsiveness HELL

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-        $('.showAnswer').click(function(){
-            $(this).parent('.support').find('.text').slideToggle(300)
+        // SHOW ANSWER ON SUPPORT.HTML
+        $('.support .showAnswer').click(function(){
+            event.preventDefault();
+            $(this).closest('.support').find('.text').slideToggle(300)
+        });
+        
+        // CLOSE NOTIFICATION
+        $('.notification .close-notification').click(function(){
+           $(this).parent().fadeOut(200); 
+        });
+        
+        // ACTIVE/INACTIVE STATE BUTTONS SETTINGS
+        $('.settings .line .active').text('ACTIVE');
+        $('.settings .line .inactive').text('INACTIVE');
+        $('.settings .line .state').click(function(){
+            $(this).toggleClass('active inactive'); 
+                if ($(this).hasClass('active')) {
+                    $(this).text('ACTIVE');
+                } else if ($(this).hasClass('inactive')){
+                    $(this).text('INACTIVE');
+                }
         });
 });
